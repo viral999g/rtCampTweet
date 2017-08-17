e772d08758fb01b704e624f2a877595c015d0d6b
Viral Gandhi <viral999g@gmail.com>
2017-08-17 22:58:48 +0530

    Update home.php

--- home.php
+++ home.php
@@ -29,14 +29,13 @@
 	<html lang="en">
 	<head>
 		<!-- Theme Made By www.w3schools.com - No Copyright -->
-		<title>Bootstrap Theme Company Page</title>
+		<title>Twitter Challenge</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="style.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
