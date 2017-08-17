02539fd3765d591d8e4a652c69b8d836297852e9
viral999g <viral999g@gmail.com>
2017-08-17 22:48:57 +0530

    nothig
    nothig

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+/* configuration */
+
+define('CONSUMER_KEY', 'bqSNqoAK19oWReczthqfAFUsN');
+define('CONSUMER_SECRET', '0XYprVCBnQcZjz8CdseGiNOfN6aXEWwlkiOwTWn21TGWtlRQal');
+define('OAUTH_CALLBACK', 'http://localhost/Final/redirect.php'); //for localhost
+
+
+?>
