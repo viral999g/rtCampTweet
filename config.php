4b7692ad16401cd7714f566543ef309bf26fbe58
Viral Gandhi <viral999g@gmail.com>
2017-08-17 22:55:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,5 @@
 
 define('CONSUMER_KEY', 'bqSNqoAK19oWReczthqfAFUsN');
 define('CONSUMER_SECRET', '0XYprVCBnQcZjz8CdseGiNOfN6aXEWwlkiOwTWn21TGWtlRQal');
-define('OAUTH_CALLBACK', 'http://localhost/Final/redirect.php'); //for localhost
-
-
+define('OAUTH_CALLBACK', 'https://tweeterchallenge.herokuapp.com//redirect.php'); //for localhost
 ?>
